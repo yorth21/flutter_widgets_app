99aaafd242a4576b3abb70355e412c0bf9c57090
Yorth H. Ortegon <yorth@udenar.edu.co>
2024-11-11 18:19:45 -0500

    feat: progress indicator finish

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -24,4 +24,9 @@ const appMenuItems = <MenuItem>[
       subTitle: 'Un contenedor estilizado',
       link: '/cards',
       icon: Icons.credit_card),
+  MenuItem(
+      title: 'Progres Indicators',
+      subTitle: 'Generales y controlados',
+      link: '/progress',
+      icon: Icons.refresh_rounded)
 ];
